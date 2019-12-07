78b3bab1942597a628633306ed25a8f2c96f4c06
Dettoni <dettonex25@gmail.com>
2019-12-07 16:37:39 -0300

    alteracao

---
+++ laser-behavior.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('laser-behavior', {
+
+    schema: {
+        speed: { default: 1 }
+    },
+
+    init: function () {
+        this.el.sceneEl.addBehavior(this);
+    },
+
+    tick: function () {
+        console.log("anee");
+        var object3D = this.el.object3D;
+        object3D.translateY(this.data.speed);
+    }
+});
